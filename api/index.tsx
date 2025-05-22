045734751b21651c84aee6d481b943524518c687
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-05-22 12:05:04 -0300

    o vercel completou o build

--- api/index.tsx
+++ api/index.tsx
@@ -1,19 +1,12 @@
-// api/index.ts
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from '../src/app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-import { Server } from 'http';
+import { IncomingMessage, ServerResponse } from 'http';
+import { createNestHandler } from '../src/main';
 
-const server = express();
-let cachedServer: Server;
+const bootstrap = createNestHandler();
 
-export default async function handler(req, res) {
-  if (!cachedServer) {
-    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-    await app.init();
-    cachedServer = server.listen(0);
-  }
-
-  return server(req, res);
+export default async function handler(
+  req: IncomingMessage,
+  res: ServerResponse,
+) {
+  const appHandler = await bootstrap;
+  return appHandler(req, res);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express'; // <-- IMPORT CORRIGIDO
+import { Express } from 'express';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+export async function createNestHandler(): Promise<Express> {
+  const expressApp = express();
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+  await app.init();
+  return expressApp;
 }
-bootstrap();
+
+// Só roda localmente com `ts-node src/main.ts`
+if (require.main === module) {
+  void startServer();
+}
+async function startServer() {
+  const server = await createNestHandler();
+  server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+  });
+}
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
